0bba6a00abd54c4b4052397f8bcb570bd9164b16
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2022-10-14 23:45:52 +0300

    Разметка и стили

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import CostItem from "./components/CostItem";
 
 function App() {
   return (
     <div >
-      <h1>
-    Изучение React!
-      </h1>
+      <h1>Изучение React!</h1>
+    <CostItem></CostItem>
     </div>
   );
 }
---
+++ src/components/CostItem.js
@@ -0,0 +1,15 @@
+import './CostItem.css'
+
+function CostItem() {
+    return (
+    <div className='cost-item'>
+        <div>Март 2021 12</div>
+        <div className='cost-item__description'>
+            <h2>Холодильник</h2>
+            <div className='cost-item__price'>$999.99</div>
+        </div>
+    </div>
+    );
+}
+
+export default CostItem;
