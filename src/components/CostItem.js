20233aa94f9a679bc6f2245ff27cf0d1b506b240
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2022-10-15 23:01:17 +0300

    компонент стилей

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,6 +1,6 @@
 import './CostItem.css'
 import CostDate from './CostDate';
-
+import Card from "./Card"
 
 function CostItem(props) {
     //const costDate = new Date(2021, 2, 12); //создаем объект даты
@@ -8,13 +8,13 @@ function CostItem(props) {
     //const costAmount = 999.99;
     
     return (
-    <div className='cost-item'>
+    <Card className='cost-item'>
         <CostDate date={props.date}/>
         <div className='cost-item__description'>
             <h2>{props.description}</h2>
             <div className='cost-item__price'>${props.amount}</div>
         </div>
-    </div>
+    </Card>
     );
 }
 
